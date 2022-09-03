3af1b4d5d7c72be8e3050a4487aeb4650e285a25
Emerson Millard <emillard.code@gmail.com>
2022-09-03 15:41:14 -0700

    Initial commit

---
+++ src/test/java/com/project/junitmockitojt/JUnitMockitoJtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.junitmockitojt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JUnitMockitoJtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
